d06ce2c42620f7ae93f1afaaefc80ff25d83ca62
ripreal <ripreal1@yandex.ru>
2023-11-02 20:43:54 +0300

    dev

--- generate-secrets.bat
+++
@@ -1,1 +0,0 @@
-kubectl create secret docker-registry mysecret --docker-server=ghcr.io --docker-username=solution8 --docker-password=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 --docker-email=mail@screener.com
